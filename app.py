bf2c6c3c327513c4c8e58b5f4553e8e2235a4ae9
thanhthao020303 <160449605+thanhthao020303@users.noreply.github.com>
2026-03-04 00:38:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,12 @@ st.set_page_config(page_title="Attendance System", layout="wide")
 # =============================
 
 if not firebase_admin._apps:
-    firebase_dict = json.loads(st.secrets["firebase"])
-    cred = credentials.Certificate(firebase_dict)
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+st.success("Firebase connected successfully!")
 
 # =============================
 # LOGIN
