25cab5f2bc8624f6414f4f026cfc436fe1bb9670
thanhthao020303 <160449605+thanhthao020303@users.noreply.github.com>
2026-03-04 00:15:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ st.set_page_config(page_title="Attendance System", layout="wide")
 # FIREBASE INIT (STREAMLIT CLOUD SAFE)
 # =============================
 if not firebase_admin._apps:
-    firebase_dict = json.loads(st.secrets["firebase"])
-    cred = credentials.Certificate(firebase_dict)
+    cred = credentials.Certificate(st.secrets["firebase"])
     firebase_admin.initialize_app(cred)
+
 
 db = firestore.client()
 
